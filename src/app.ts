3d01765eb4167e7e4a05d366d0b87e6995c12fb2
Avinash Kumar <avinash.k95347@gmail.com>
2024-09-29 12:31:47 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 app.use(express.urlencoded({ extended: true }));
 
 
